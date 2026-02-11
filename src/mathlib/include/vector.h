ea7d73642f634f1990320ad7f39681ea11284d69
AlaXar (cUrios) <alazardemessie>
2026-02-11 08:43:51 +0300

    feature: all vector operations are implemented

--- src/mathlib/include/vector.h
+++ src/mathlib/include/vector.h
@@ -8,12 +8,18 @@ public:
     Vector3(double x, double y, double z);
     ~Vector3();
 
-    // dot product
-    double dot(const Vector3 &other) const;
 
     double magnitude() const;
     double lengthSquared() const;
     Vector3 normalize() const;
+
+    // Vector operations
+    double dot(const Vector3 &other) const;
+    Vector3 cross(const Vector3& other) const;
+    Vector3 operator*(const Vector3 &other) const;
+    Vector3 operator*(double scalar) const;
+    Vector3 operator-(const Vector3 &other) const;
+    Vector3 operator+(const Vector3 &other) const;
     
 private:
     double x, y, z;
--- src/mathlib/src/vector.cpp
+++ src/mathlib/src/vector.cpp
@@ -29,3 +29,22 @@ Vector3 Vector3::normalize() const {
 
     return Vector3(x * invertedLength, y * invertedLength, z * invertedLength);
 }
+
+Vector3 Vector3::operator*(double scalar) const {
+    return Vector3(x * scalar, y * scalar, z * scalar);
+}
+
+Vector3 Vector3::operator*(const Vector3& other) const {    // component wise (hadamard product)
+    return Vector3(x * other.x, y * other.y, z * other.z);
+}
+Vector3 Vector3::operator+(const Vector3& other) const {
+    return Vector3(x + other.x, y + other.y, z * other.z);
+}
+
+Vector3 Vector3::operator-(const Vector3& other) const {
+    return Vector3(x - other.x, y - other.y, z - other.z);
+}
+
+Vector3 Vector3::cross(const Vector3& other) const {
+    return Vector3(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x);
+}
